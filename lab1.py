e399874d785a4618aa850c5a5fb130fbd4afc6d6
jiafengwu0301 <jack.wu1224@gmail.com>
2016-09-08 14:56:33 -0600

    lab01

---
+++ lab1.py
@@ -0,0 +1,5 @@
+import requests
+#response = requests.get("http://google.com")
+response = requests.post("http://ccid-eddieantonio.rhcloud.com/jiafeng1")
+print response.status_code
+#print requests.__version__
