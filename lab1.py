7f65ab5baba48638d3b14fc34e8db4e87f9e5a71
jiafengwu0301 <jack.wu1224@gmail.com>
2016-09-08 15:01:23 -0600

    edit lab01

--- lab1.py
+++ lab1.py
@@ -1,5 +1,6 @@
 import requests
 #response = requests.get("http://google.com")
-response = requests.post("http://ccid-eddieantonio.rhcloud.com/jiafeng1")
-print response.status_code
+#response = requests.post("http://ccid-eddieantonio.rhcloud.com/jiafeng1")
+response = requests.get("https://github.com/jiafengwu0301/CMPUT404Lab01/raw/master/lab1.py")
+print response.text
 #print requests.__version__
